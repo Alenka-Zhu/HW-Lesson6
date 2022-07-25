de8461a0f3824a6a6a97d3ed4b04a17498d4dfcb
Snezhana Poly <9217478967@mail.ru>
2022-07-25 23:14:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            foreach (char c in str)
+            {
+                str2 = c + str2;
+            }
+            Console.WriteLine(str2);
+            if (String.Equals(str2, str))
+            {
+                Console.WriteLine("вы ввели палиндром");
+            }
+            else
+                Console.WriteLine("это не палиндром");
+            Console.ReadKey();
+        }
+    }
+}
