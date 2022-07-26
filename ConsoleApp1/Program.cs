eb4b96bec28ea1f2df3bd8584e5efe2a1ebd998f
Snezhana Poly <9217478967@mail.ru>
2022-07-26 10:26:20 +0300

    исправлено

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,13 +11,17 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string str = Console.ReadLine();
-            string str2 = "";
-            foreach (char c in str)
+            str = str.Replace(" ", "");/*убрали пробелы*/
+            str = str.ToLower();/*привели к нижнему регистру*/
+            string str2 = "";/*завели переменную для перевернутой строки*/
+            foreach (char c in str)/*перебираем str,прибавляя в str2 каждый последуюющий знак перед строкой,чтобы
+                                    * получить перевертыш*/
             {
                 str2 = c + str2;
             }
-            Console.WriteLine(str2);
-            if (String.Equals(str2, str))
+            Console.WriteLine(str2);/*вывели перевертыш*/
+            if (String.Equals(str2, str))/*если строки равны, то выводим,что это палиндром
+                                          * можно так же: if(str2==str)*/
             {
                 Console.WriteLine("вы ввели палиндром");
             }
